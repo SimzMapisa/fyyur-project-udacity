b35bf60f1e4138bd7f087d383309a3d6ac8ddb1f
SimzMapisa <simbarashe795@gmail.com>
2022-06-28 01:25:46 +0200

    made tiny changes like adding the csrf tag to the html forms

--- migrations/versions/be0d6875c105_.py
+++ migrations/versions/be0d6875c105_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 4d129d3fa4e1
+Revision ID: be0d6875c105
 Revises: 
-Create Date: 2022-06-24 06:44:10.311177
+Create Date: 2022-06-27 05:21:28.637590
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '4d129d3fa4e1'
+revision = 'be0d6875c105'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -24,7 +24,7 @@ def upgrade():
     sa.Column('city', sa.String(length=120), nullable=True),
     sa.Column('state', sa.String(length=120), nullable=True),
     sa.Column('phone', sa.String(length=120), nullable=True),
-    sa.Column('genres', sa.String(length=120), nullable=True),
+    sa.Column('genres', sa.ARRAY(sa.String()), nullable=True),
     sa.Column('image_link', sa.String(length=500), nullable=True),
     sa.Column('facebook_link', sa.String(length=120), nullable=True),
     sa.Column('website', sa.String(length=120), nullable=True),
